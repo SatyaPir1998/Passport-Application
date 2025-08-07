de334c5b43b2f0602a82884d340371d29475ec8a
Satyapir Ghosh <137476888+SatyaPir1998@users.noreply.github.com>
2025-08-07 10:44:14 +0530

    Add data model: Applicant

---
+++ core/data-model/Applicant.js
@@ -0,0 +1,42 @@
+module.exports = {
+  id: "Applicant",
+  label: "Applicant",
+  description: "",
+  fields: [
+  {
+    "id": "1",
+    "name": "Name",
+    "type": "string",
+    "required": true,
+    "description": "Name of the applicant"
+  },
+  {
+    "id": "2",
+    "name": "DateOfBirth",
+    "type": "date",
+    "required": true,
+    "description": "Date of birth of the applicant"
+  },
+  {
+    "id": "3",
+    "name": "Nationality",
+    "type": "string",
+    "required": true,
+    "description": "Nationality of the applicant"
+  },
+  {
+    "id": "4",
+    "name": "IdentityProof",
+    "type": "string",
+    "required": true,
+    "description": "Identity proof document"
+  },
+  {
+    "id": "5",
+    "name": "AddressProof",
+    "type": "string",
+    "required": true,
+    "description": "Address proof document"
+  }
+]
+};
